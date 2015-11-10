cdabc296c3d09957bdad1baea3949eac888725c3
yinjimmy <jimmy.yin5@gmail.com>
2015-11-10 17:43:29 +0800

    update js,lua sample

--- js/src/app.js
+++ js/src/app.js
@@ -12,11 +12,25 @@ var HelloWorldLayer = cc.Layer.extend({
         // ask the window size
         var size = cc.winSize;
 
-        var coinsLabel = cc.Label.createWithSystemFont("Hello World", "Arial", 64);
-        coinsLabel.setPosition(size.width/2, size.height/2);
+        var coinsLabel = cc.Label.createWithSystemFont("Hello Js", "Arial", 64);
+        coinsLabel.setPosition(size.width/2, size.height/5);
         this.addChild(coinsLabel);
 
+        cc.MenuItemFont.setFontName('arial');
+        cc.MenuItemFont.setFontSize(32);
+
+        var menu = new cc.Menu(
+            new cc.MenuItemFont("clickme", this.onClicked, this)
+            );
+        menu.setPosition(size.width/2, size.height/2);
+        menu.alignItemsVerticallyWithPadding(20);
+        this.addChild(menu);
+
+
         return true;
+    },
+    onClicked: function() {
+        console.log("on click")
     }
 });
 
--- lua/src/app/views/MainScene.lua
+++ lua/src/app/views/MainScene.lua
@@ -5,8 +5,18 @@ local MainScene = class("MainScene", cc.load("mvc").ViewBase)
 
 function MainScene:onCreate()
 
-    cc.Label:createWithSystemFont("Hello World", "Arial", 64)
-    		:move(display.cx, display.cy)
+	cc.MenuItemFont:setFontName("Arial")
+    cc.Menu:create(
+                   cc.MenuItemFont:create("clickme"):onClicked(function()
+                        print("on click")
+                    end)
+                   )
+        :move(display.cx, display.cy)
+        :addTo(self)
+        :alignItemsVerticallyWithPadding(20)
+
+    cc.Label:createWithSystemFont("Hello Lua", "Arial", 64)
+    		:move(display.cx, display.height/5)
 		    :addTo(self)
 end
 
